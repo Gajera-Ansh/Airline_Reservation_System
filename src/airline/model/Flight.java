abfdf5af7742bb285155815f89b994719bb06cdd
Ansh Gajera <gajeraansh4227@gmail.com>
2025-07-14 23:07:08 +0530

    Initial commit

--- src/airline/model/Flight.java
+++ src/airline/model/Flight.java
@@ -1,4 +1,30 @@
 package airline.model;
 
+import java.time.LocalDateTime;
+
 public class Flight {
+    int flight_id;
+    String flight_number;
+    String departure;
+    String destination;
+    LocalDateTime departure_time;
+    LocalDateTime arrival_time;
+    int total_seats;
+    int available_seats;
+    double price;
+    int admin_id;
+
+    public Flight(int flight_id, String flight_number, String departure, String destination, LocalDateTime departure_time,
+                  LocalDateTime arrival_time, int total_seats, int available_seats, double price, int admin_id) {
+        this.flight_id = flight_id;
+        this.flight_number = flight_number;
+        this.departure = departure;
+        this.destination = destination;
+        this.departure_time = departure_time;
+        this.arrival_time = arrival_time;
+        this.total_seats = total_seats;
+        this.available_seats = available_seats;
+        this.price = price;
+        this.admin_id = admin_id;
+    }
 }
--- src/airline/model/Passenger.java
+++ src/airline/model/Passenger.java
@@ -1,4 +1,15 @@
 package airline.model;
 
 public class Passenger {
+    int passenger_id;
+    String name;
+    String email;
+    String phone;
+
+    public Passenger(int passenger_id, String name, String email, String phone) {
+        this.passenger_id = passenger_id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 }
