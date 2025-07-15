9ae6a0631cf4c01cd7dfc40735b6638a7d8e9fa0
Ansh Gajera <gajeraansh4227@gmail.com>
2025-07-15 20:29:22 +0530

    15/07 changes

--- src/airline/util/DBUtil.java
+++ src/airline/util/DBUtil.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBUtil {
     public static final String URL = "jdbc:mysql://localhost:3306/airline_db";
     public static final String USER = "root";
-    public static final String PASSWORD = "yourpassword";
+    public static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
