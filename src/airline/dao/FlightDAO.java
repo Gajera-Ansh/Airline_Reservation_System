8328fe0d1f5651ca31e27ed095a142a444eba263
Gajera-Ansh <gajeraansh4227@gmail.com>
2025-07-30 19:25:02 +0530

    30/07 Admin can view all the flights which is they control

--- src/airline/dao/FlightDAO.java
+++ src/airline/dao/FlightDAO.java
@@ -20,8 +20,8 @@ public class FlightDAO {
         try {
             String sql = "UPDATE flights SET departure_time = DATE_ADD(departure_time, INTERVAL 1 DAY), arrival_time = DATE_ADD(arrival_time, INTERVAL 1 DAY)";
 //            String sql = "UPDATE flights SET departure_time = departure_time - INTERVAL 1 DAY, arrival_time = arrival_time - INTERVAL 1 DAY;";
-            Statement st = DBUtil.con.createStatement();
-            st.executeUpdate(sql);
+//            Statement st = DBUtil.con.createStatement();
+//            st.executeUpdate(sql);
         } catch (Exception _) {
         }
     }
