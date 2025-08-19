7dfb895c279eee7f7e0c5afe96f4dc9f381a6f90
Gajera-Ansh <gajeraansh4227@gmail.com>
2025-08-19 20:45:14 +0530

    Checking all the input

--- src/airline/util/DBUtil.java
+++ src/airline/util/DBUtil.java
@@ -2,6 +2,8 @@ package airline.util;
 
 import java.sql.*;
 
+import airline.App;
+
 public class DBUtil {
     public static final String URL = "jdbc:mysql://localhost:3306/airline_db";
     public static final String USER = "root";
@@ -15,9 +17,9 @@ public class DBUtil {
         } catch (SQLException _) {
         }
         if (con != null) {
-            System.out.println("Database connection established successfully.");
+            System.out.println(App.green + "Database connection established successfully." + App.reset);
         } else {
-            System.out.println("Failed to establish database connection.");
+            System.out.println(App.red + "Failed to establish database connection." + App.reset);
         }
     }
 }
